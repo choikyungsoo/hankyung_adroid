74d230291e80da8e9aee2d98a36a608436a149d1
JiWon <jiohning00@hanmail.net>
2021-07-12 23:34:18 +0900

    first commit

---
+++ app/src/main/java/com/team_noyeah/hankyung_adroid/MainActivity.java
@@ -0,0 +1,14 @@
+package com.team_noyeah.hankyung_adroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
